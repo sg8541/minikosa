aaf2a4b2b80120996266159fdb4c53336168e69b
sg8541 <loggingpj@gmail.com>
2024-10-14 12:13:13 +0900

    Feat: 추가_ 프로젝트 생성
    프로젝트 생성

---
+++ src/test/java/com/kosa/mini/MiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
